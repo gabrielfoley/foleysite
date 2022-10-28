3ee72de954d9124dce4fff853cb5eb1f490c8b60
gabrielfoley <80079989+gabrielfoley@users.noreply.github.com>
2022-10-28 09:53:43 -0700

    fixed some bugs slug,stripe

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -1,4 +1,6 @@
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+import Stripe from 'stripe';
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
@@ -13,21 +15,34 @@ export default async function handler(req, res) {
           {shipping_rate: 'shr_1Lxg1YDYzJ2xtZLyBwnxalIV'},
         ],
 
-        line_items: [
-          {
-            // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-            price: '{{PRICE_ID}}',
-            quantity: 1,
-          },
-        ],
-        mode: 'payment',
+        line_items: req.body.cartItems.map((item) => {
+          const img = item.image[0].asset._ref;
+          const newImage = img.replace('image-', 'https://cdn.sanity.io/images/2qqwxpih/production/').replace('-webp', '.webp');
+
+          return {
+            price_data: {
+              currency: 'usd',
+              product_data: {
+                name: item.name,
+                images: [newImage],
+              },
+              unit_amount: item.price * 100,
+            },
+            adjustable_quantity: {
+              enabled: true,
+              minimum: 1,
+            },
+            quantity: item.quantity
+          }
+      }),
+    
         success_url: `${req.headers.origin}/?success=true`,
         cancel_url: `${req.headers.origin}/?canceled=true`,
         automatic_tax: {enabled: true},
       }
       // Create Checkout Sessions from body params.
       const session = await stripe.checkout.sessions.create(params);
-      res.redirect(303, session.url);
+      res.status(200).json(session);
     } catch (err) {
       res.status(err.statusCode || 500).json(err.message);
     }
